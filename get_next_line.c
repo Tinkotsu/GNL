bf8f7774d494f95d33ff494e1038fcc3c8cf2f9f
Tinkotsu <tinkotsu@gmail.com>
2019-10-13 23:36:48 +0300

    12

--- get_next_line.c
+++ get_next_line.c
@@ -3,99 +3,96 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ifran <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: tinkotsu <tinkotsu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/13 16:22:38 by ifran             #+#    #+#             */
-/*   Updated: 2019/10/13 16:22:41 by ifran            ###   ########.fr       */
+/*   Updated: 2019/10/13 23:36:21 by tinkotsu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static	t_list	*s_check_and_create(t_list **list, int fd)
+static	t_list	    *s_check_and_create(t_list **list, int fd)
 {
-	t_file	*file;
-	t_list	*temp;
+    t_file	*file;
+    t_list	*temp;
 
-	if (fd < 0)
+    if (fd < 0)
 		return (NULL);
-	temp = *list;
-	while (temp)
-	{
+    temp = *list;
+    while (temp)
+    {
 		if (((t_file *)temp->content)->fd == fd)
-			return (temp);
+		    return (temp);
 		temp = temp->next;
-	}
-	if (!(file = (t_file *)malloc(sizeof(t_file))))
+    }
+    if (!(file = (t_file *)malloc(sizeof(t_file))))
 		return (NULL);
-	file->str = NULL;
-	file->fd = fd;
-	ft_lstadd(list, ft_lstnew(file, sizeof(t_file)));
-	return (*list);
+    file->str = NULL;
+    file->fd = fd;
+    ft_lstadd(list, ft_lstnew(file, sizeof(t_file)));
+    return (*list);
 }
 
-static int		get_buff(t_list *list, char **buff)
+static int	    get_buff(t_list *list, char **buff)
 {
-	char	*temp;
-	int		res;
+    char	*temp;
+    int		res;
 
-	res = 0;
-	temp = ((t_file *)list->content)->str;
-	if (temp)
-	{
+    res = 0;
+    temp = ((t_file *)list->content)->str;
+    if (temp)
+    {
 		((t_file *)list->content)->str = ft_strjoin(temp, *buff);
 		ft_strdel(&temp);
-	}
-	else
+    }
+    else
 		((t_file *)list->content)->str = ft_strdup(*buff);
-	if (ft_strchr(((t_file *)list->content)->str, '\n'))
+    if (ft_strchr(((t_file *)list->content)->str, '\n'))
 		res = 1;
-	ft_strclr(*buff);
-	return (res);
+    ft_strclr(*buff);
+    return (res);
 }
 
-static int		push_line(t_list *list, char **line)
+static int	    push_line(t_list *list, char **line)
 {
-	char	*temp;
-	char	*str;
+    char	*temp;
+    char	*str;
 
-	if (!(str = ((t_file *)list->content)->str))
+    if (!(str = ((t_file *)list->content)->str))
 		return (0);
-	if ((temp = ft_strchr(str, '\n')))
-	{
-		*line = ft_strsub(str, 0, ft_strlen(str) - ft_strlen(temp));
-		if (*(++temp))
-			((t_file *)list->content)->str = ft_strdup(temp);
-		else
-			((t_file *)list->content)->str = NULL;
-	}
-	else
-	{
+    if (!(temp = ft_strchr(str, '\n')))
 		*line = ft_strdup(str);
-		((t_file *)list->content)->str = NULL;
-	}
-	ft_strdel(&str);
-	return (1);
+    else
+    {
+		*line = ft_strsub(str, 0, ft_strlen(str) - ft_strlen(temp));
+		if (!(*(++temp)))
+	    	temp = NULL;
+		else
+		    temp = ft_strdup(temp);
+    }
+    ((t_file *)list->content)->str = temp;
+    ft_strdel(&str);
+    return (1);
 }
 
-int				get_next_line(const int fd, char **line)
+int		    get_next_line(const int fd, char **line)
 {
-	int				c;
-	char			*buff;
-	static	t_list	*list;
-	t_list			*curr;
+    int		    c;
+    char	    *buff;
+    static  t_list  *list;
+    t_list	    *curr;
 
-	*line = NULL;
-	buff = ft_strnew(BUFF_SIZE);
-	if (!(curr = s_check_and_create(&list, fd)))
+    buff = ft_strnew(BUFF_SIZE);
+    if (!(curr = s_check_and_create(&list, fd)))
 		return (-1);
-	while ((c = read(fd, buff, BUFF_SIZE)) > 0)
+    while ((c = read(fd, buff, BUFF_SIZE)) > 0)
 		if (get_buff(curr, &buff))
-			break ;
-	ft_strdel(&buff);
-	if (c < 0)
+		    break ;
+    ft_strdel(&buff);
+    if (c < 0)
 		return (-1);
-	if (push_line(curr, line))
+    if (push_line(curr, line))
 		return (1);
-	return (0);
+    return (0);
 }
